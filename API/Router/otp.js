a40082ab08cea253145ecbe2981a82b9eeb0669d
viethoang5426 <caohoang5969@gmail.com>
2024-12-12 14:17:24 +0700

    Seconf commit 12/12

--- API/Router/otp.js
+++ API/Router/otp.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const controlerOTP = require("../Controller/otp");
 
 router.post("/sendotp", controlerOTP.sendotp);
+router.post("/verifyotp", controlerOTP.checkOTP);
 
 module.exports = router;
