2d1bd69ab5f0b7e80029dabd9ccc358000384061
viethoang5426 <caohoang5969@gmail.com>
2024-12-16 10:43:42 +0700

    Sign Up API Update

--- API/Model/user.js
+++ API/Model/user.js
@@ -1,23 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const user = new mongoose.Schema({
-    user : {
-        type : String,
-        required : true ,
+const user = new mongoose.Schema(
+  {
+    user: {
+      type: String,
+      required: true,
     },
-    password : {
-        type : String,
-        required : true ,
+    password: {
+      type: String,
+      required: true,
     },
-    email : {
-        type : String,
-        required : true ,
-        unique : true ,
-    }
-},
-{
-    timestamps : true ,
-},
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    schoolName: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    dateBirth: {
+      type: Date,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("user",user)
+module.exports = mongoose.model("user", user);
